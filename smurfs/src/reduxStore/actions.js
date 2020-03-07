121d985fc023d4f63344ecb9fd1cfcf4dfee61e6
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-06 20:03:56 -0500

    refactored FETCH to GET as appropriate

--- smurfs/src/reduxStore/actions.js
+++ smurfs/src/reduxStore/actions.js
@@ -1,9 +1,9 @@
-//our state management system is Redux, so we add Actions here to for gettting API data
-export const FETCH_API_START = 'FETCH_API_START';
-export const FETCH_API_SUCCESS = 'FETCH_API_SUCCESS';
-export const FETCH_API_FAILURE = 'FETCH_API_FAILURE';
+//our state management system is Redux, so we add Actions here to for GET_API data
+export const GET_API_START = 'GET_API_START';
+export const GET_API_SUCCESS = 'GET_API_SUCCESS';
+export const GET_API_FAILURE = 'GET_API_FAILURE';
 
-//looks like our required is add a Smurf, so call them POST_API to differ from FETCH ~silly~
+//looks like our required is add a Smurf, so call them POST_API data to differ from GET
 export const POST_API_START = 'POST_API_START'; //THIS IS FOR SMURF
 export const POST_API_SUCCESS = 'POST_API_SUCCESS'; //STILL SMURF
 export const POST_API_FAILURE = 'POST_API_FAILURE';
