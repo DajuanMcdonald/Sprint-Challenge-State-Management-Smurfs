a124331a8413480ac62bbdce6c16f27922c50f0c
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-06 20:01:22 -0500

    refactored App.js for inline class syntax

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+        <React.Fragment>
+            <h1>Le Pays Maudit</h1>
+
+        </React.Fragment>
     );
   }
 }
