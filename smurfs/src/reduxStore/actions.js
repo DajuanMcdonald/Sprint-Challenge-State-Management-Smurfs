e4e618752b5448a40559c7e71b3c5f59bcfb1cf0
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-06 19:46:08 -0500

    add actions.js

---
+++ smurfs/src/reduxStore/actions.js
@@ -0,0 +1,10 @@
+//our state management system is Redux, so we add Actions here to for gettting API data
+export const FETCH_API_START = 'FETCH_API_START';
+export const FETCH_API_SUCCESS = 'FETCH_API_SUCCESS';
+export const FETCH_API_FAILURE = 'FETCH_API_FAILURE';
+
+//looks like our required is add a Smurf, so call them POST_API to differ from FETCH ~silly~
+export const POST_API_START = 'POST_API_START'; //THIS IS FOR SMURF
+export const POST_API_SUCCESS = 'POST_API_SUCCESS'; //STILL SMURF
+export const POST_API_FAILURE = 'POST_API_FAILURE';
+
